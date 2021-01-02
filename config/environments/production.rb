487ad2bf7b0636fc9c0150a5887c9bdec5e92bca
Hajime Yamaguchi <h_yamaguchi@chaintope.com>
2021-01-03 00:32:40 +0900

    Change protocol used in devise mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,5 +119,6 @@ Rails.application.configure do
     domain: 'heroku.com',
     enable_starttls_auto: true
   }
-  config.action_mailer.default_url_options = { :host => 'tapyrus-platform.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'tapyrus-platform.herokuapp.com', :protocol => 'https'}
+  config.mailer
 end
